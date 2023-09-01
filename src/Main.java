214cc6963548cd046d266826d3193931270159f9
shibuya-n <a.duong2006@gmail.com>
2023-09-01 11:08:04 -0700

    test

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,40 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.Scanner;
+
+    import java.util.*;
+
+//In this project, you'll design and implement a Java program for a number guessing game with multiple difficulty levels. Players will try to guess a randomly generated number within different ranges based on the chosen difficulty level. Project parameters:
+//        User Interaction: Prompt the user to play and choose a difficulty level.
+//        Game Logic: Generate a random number within the selected difficulty's range. Allow the player to input their guess and compare it to the generated number. Implement logic to determine if the guess is correct or incorrect and tell the user higher or lower.
+
+    public class Main {
+        public static void main(String[] args) {
+            boolean yesNo = ask();
+            if (yesNo)  {
+                System.out.println("play");
+            }
+            else {
+                ask();
+            }
+
+        }
+        public static boolean ask(){
+            Scanner play = new Scanner(System.in);
+            System.out.println("Play? (Yes/No)");
+            String x = play.nextLine();
+
+            if (x.toLowerCase().equals("yes")) {
+                return true;
+            }
+            else if (x.toLowerCase().equals("no")) {
+                return false;
+            }
+
+            else {
+                return ask();
+            }
+
+        }
+
     }
-}
+
+
